02476165db857c71644756cfb805e3cf78df6d9b
nithishselva47 <127393038+nithishselva47@users.noreply.github.com>
2023-07-26 01:42:48 -0700

    halfsubractor_test.v

---
+++ half subtractor/halfsubractor_test.v
@@ -0,0 +1,33 @@
+module halfsubtractor_test;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire d;
+	wire bo;
+
+	// Instantiate the Unit Under Test (UUT)
+	halfsubtractor uut (
+		.a(a), 
+		.b(b), 
+		.d(d), 
+		.bo(bo)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100 a = 0;b = 1;
+		#100 a = 1;b = 0;
+		#100 a = 1;b = 1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
