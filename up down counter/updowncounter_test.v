2831dc51d975a65df9a94918dc15fa3d080e1622
nithishselva47 <127393038+nithishselva47@users.noreply.github.com>
2023-07-27 23:21:21 -0700

    updowncounter_test.v

---
+++ up down counter/updowncounter_test.v
@@ -0,0 +1,32 @@
+module updowncounter_test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg up_down;
+
+	// Outputs
+	wire [3:0] counter;
+
+	// Instantiate the Unit Under Test (UUT)
+	updowncounter uut (
+		.clk(clk), 
+		.reset(reset), 
+		.up_down(up_down), 
+		.counter(counter)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk=0;
+forever #50 clk=~clk;
+end
+initial begin
+reset=1; up_down=0;
+#20 reset=0; up_down=1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
