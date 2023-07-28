8bb542d2d71bb1891487bb9d7f79d0e59c96fca6
nithishselva47 <127393038+nithishselva47@users.noreply.github.com>
2023-07-28 00:18:48 -0700

    magnitudecomparator_test.v

---
+++ magnitude comparator/magnitudecomparator_test.v
@@ -0,0 +1,36 @@
+module magnitudecomparator_test;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire x;
+	wire y;
+	wire z;
+
+	// Instantiate the Unit Under Test (UUT)
+	magnitudecomparator uut (
+		.a(a), 
+		.b(b), 
+		.x(x), 
+		.y(y), 
+		.z(z)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100 a = 0;b = 1;
+        #100 a = 1;b = 0;
+		  #100 a = 1;b = 1;
+		// Add stimulus here
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
